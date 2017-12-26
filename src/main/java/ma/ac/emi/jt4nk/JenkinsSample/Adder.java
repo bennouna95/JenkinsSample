26a4f207dd755602ad1dd92d7ffb3351aee15488
Joël Tankam <joel.tankam@gmail.com>
2017-12-26 09:24:21 +0000

    Initial commit

---
+++ src/main/java/ma/ac/emi/jt4nk/JenkinsSample/Adder.java
@@ -0,0 +1,9 @@
+package ma.ac.emi.jt4nk.JenkinsSample;
+
+import java.util.Arrays;
+
+public class Adder {
+    public static int add(int a, int ...b){
+        return a + Arrays.stream(b).sum();
+    }
+}
---
+++ src/test/java/ma/ac/emi/jt4nk/JenkinsSample/AdderTest.java
@@ -0,0 +1,16 @@
+package ma.ac.emi.jt4nk.JenkinsSample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AdderTest {
+
+    @Test
+    public void add() {
+        assertEquals("Basic addition", 2, Adder.add(1, 1));
+        assertEquals("Multiple additions", 5, Adder.add(1, 1, 1, 1, 1));
+        assertEquals("No second term", 1, Adder.add(1));
+        assertEquals("Negative values", -2, Adder.add(-1,-1));
+    }
+}
